85c1a85af82254b2beaac3fc78b5898757ceef91
Rohit-96522 <ponnanarohit507@gmail.com>
2026-02-25 19:36:05 +0530

    mongo db cconnection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+
+// Force Google DNS for reliable MongoDB Atlas SRV record resolution
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const connectDB = async () => {
     try {
